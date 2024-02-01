2faf6102f3790cc3d273b68bbd7d74ab60da5702
alphonce maramba <marambaalphonce@gmail.com>
2024-02-01 14:24:56 +0300

    this is the last commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// DOM Manipulation
+
+// GetElementById()
+ /*const title = document.getElementById('main-heading');
+ console.log(title);
+
+// GetElementByClassName()
+
+ const listItem = document.getElementsByClassName('list-items');
+console.log(listItem);
+
+// getElementByTagName()
+const listItem = document.getElementsByTagName('li');
+console.log(listItem);
+
+//querySelector()
+const container = document.querySelector('div');
+console.log(container);
+
+// querySelecorAll()
+
+const listItem = document.querySelectorAll('.list-items')
+for (i=0; i<listItem.length; i++) {
+    listItem[i].style.color = 'red';
+} 
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+ul.append(li)
+li.innerText = 'The Jumanjee';
+li.setAttribute('class', 'list-items');
+li.classList.add('list-items');
+console.log(li.classList.contains('list-items'));*/
+
+//const title = document.querySelector('h1');
+
+//console.log(title.getAttribute('id'));
+
+//const firstListItem = document.querySelector('.list-items');
+//console.log(firstListItem.innerText);
+//console.log(firstListItem.innerHTML);
+//console.log(firstListItem.textContent);
+
+const button1 = document.querySelector('button');
+console.log(button1);
+
+function meclick () {
+    alert.button1 = 'how are you';
+}
+
+
